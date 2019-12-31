39c45ec10aaa4ff84328c42029a0da7af9551e2e
Leo1989 <54112506+Leo1989666@users.noreply.github.com>
2019-12-31 10:40:09 +0700

    Quick Tool Bar utils

---
+++ QuickMenuBarToolEditor.cs
@@ -0,0 +1,27 @@
+﻿#if UNITY_EDITOR
+
+using System.Diagnostics;
+using System.IO;
+using UnityEditor;
+using UnityEngine;
+
+public class QuickMenuBarToolEditor
+{
+    [MenuItem("DontShootingAlone/Utils/OpenPersistentDataPath")]
+    public static void Util_OpenPersistentDataPath()
+    {
+        string path = Application.persistentDataPath;
+        EditorUtility.RevealInFinder(path);
+    }
+    
+    [MenuItem("DontShootingAlone/Utils/DeleteSaveFile")]
+    public static void Util_DeleteSaveFile()
+    {
+        string path = Application.persistentDataPath + "/savegamedata/";
+        if (Directory.Exists(path))
+        {
+            Directory.Delete(path, true);
+        }
+    }
+}
+#endif

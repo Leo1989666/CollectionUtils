29922f710889ef458e2f664ce6036b8868711142
Leo1989 <54112506+Leo1989666@users.noreply.github.com>
2020-11-29 19:24:08 +0700

    Add iOSnativeDebugFlag

---
+++ iOSNativeDebugFlagWrapper/iOSNativeDebugFlag.mm
@@ -0,0 +1,25 @@
+﻿#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+
+extern "C"
+{
+    // For General Debug.Log
+    bool GetEnableDebugLogFlag()
+    {
+        return YES;
+    }
+    ////
+    
+    // For Enabled SRDebugger
+    bool GetEnableSRDebuggerFlag()
+    {
+        return YES;
+    }
+    
+    // For Analytics
+    bool GetEnableAnalyticsFlag()
+    {
+        return YES;
+    }    
+    ////
+}
---
+++ iOSNativeDebugFlagWrapper/iOSNativeDebugFlagWrapper.cs
@@ -0,0 +1,16 @@
+﻿namespace iOSNativeDebug
+{
+    using System.Runtime.InteropServices;
+
+    public static class iOSNativeDebugFlagWrapper
+    {
+        [DllImport("__Internal")]
+        public static extern bool GetEnableDebugLogFlag();
+
+        [DllImport("__Internal")]
+        public static extern bool GetEnableSRDebuggerFlag();
+
+        [DllImport("__Internal")]
+        public static extern bool GetEnableAnalyticsFlag();
+    }    
+}
